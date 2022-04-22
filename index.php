971b2ef7666ae50f35eed403e79ad5c9c937bf73
Javon_F1996 <javonf1996@gmail.com>
2022-04-22 18:38:05 -0400

    Updated PHP Code

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <?php 
+/**
+ *  Plugin Name: Page Refresh Counter (revised)
+ *  Description: Counts how many times the page has reloaded
+ */
 
 session_start();
 // Begins session 
-if (!isset($_SESSION['count'])) {
-// If the $_SESSION does not have 'count' set as a variable
-    $_SESSION['count'] = 0;
-    // Create variable and set 'count' to 0
-} else {
-    $_SESSION['count'] ++;
-    // Increase 'count' by increments
-}
+//adds option to options.php
 
-echo $_SESSION['count'];
+function reload_count()  {
+    if (!(get_option('reload_counter','0'))) {
+    // If the $_SESSION does not have 'count' set as a variable
+        add_option('reload_counter','0');
+        // Create variable and set 'count' to 0
+    } else {
+        $count = get_option('reload_counter');
+        $count++;
+        update_option('reload_counter',"$count");
+        // Increase 'count' by increments
+    }
+    
+    echo get_option('reload_counter');
+    
+};
+
+add_action('wp_head','reload_count');
+
+
 
 ?>
